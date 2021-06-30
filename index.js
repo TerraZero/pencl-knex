9b359d487fcef04c716105e859124c83cf5f9a0a
TerraZero <terrazerocore@gmail.com>
2021-06-30 10:42:17 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Core = require('pencl-core').boot;
+const Core = require('pencl-core');
 const Knex = require('knex');
 const PenclPlugin = require('pencl-core/src/Boot/PenclPlugin');
 
@@ -21,7 +21,7 @@ class PenclKnex extends PenclPlugin {
     this._connections = {};
     this._env = this.config.env;
 
-    this.config.data = require(Core.getPath(this.config.file));
+    this.config.data = require(Core().boot.getPath(this.config.file));
   }
 
   /** @returns {Knex} */
