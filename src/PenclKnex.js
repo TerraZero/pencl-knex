913a2e922973cc62401473e3e2e667d3ba2b94e0
TerraZero <terrazerocore@gmail.com>
2021-07-12 22:35:08 +0200

    update

--- src/PenclKnex.js
+++ src/PenclKnex.js
@@ -36,7 +36,7 @@ module.exports = class PenclKnex extends PenclPlugin {
     this.config.data = require(this.boot.getPath(this.config.file));
   }
 
-  /** @returns {SchemaManager} */
+  /** @type {SchemaManager} */
   get schemas() {
     if (this._schemas === null) {
       this._schemas = new SchemaManager(this);
